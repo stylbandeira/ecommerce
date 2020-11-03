0f8cde7e5130b26c3705c936cb572ff589d12359
Styl Bandeira <stylbandeira@hotmail.com>
2020-11-03 15:15:43 -0300

    Tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -6,11 +6,14 @@ class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
 	public function __construct($opts = array(), $tpl_dir = "/views/")
 	{	
+		$this->defaults["data"]["session"] = ($_SESSION) ? $_SESSION : [];
 		$this->options = array_merge($this->defaults, $opts);
 		// config
 		$config = array(
@@ -24,7 +27,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -43,7 +46,7 @@ class Page {
 
 	public function __destruct()
 	{
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 	}
 }
  ?>
